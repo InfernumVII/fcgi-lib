b78798aa1928d1140f5a9e53c832e3d8506f8a01
InfenrumVII <egor554shved@gmail.com>
2025-09-24 04:37:28 +0300

    first commit

---
+++ src/FCGIContext.java
@@ -0,0 +1,66 @@
+import java.nio.ByteBuffer;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FCGIContext {
+    private int role;
+    private int flag;
+    private ConcurrentHashMap<String, String> params = new ConcurrentHashMap<>();
+    private ByteBuffer stdinData;
+    private boolean isReady;
+
+    public FCGIContext() {
+    }
+    
+    
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public ConcurrentHashMap<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(ConcurrentHashMap<String, String> params) {
+        this.params = params;
+    }
+
+    public ByteBuffer getStdinData() {
+        return stdinData;
+    }
+
+    public void setStdinData(ByteBuffer stdinData) {
+        this.stdinData = stdinData;
+    }
+
+    @Override
+    public String toString() {
+        return "FCGIContext [role=" + role + ", flag=" + flag + ", params=" + params.size() + ", stdinData=" + stdinData.capacity() + "]";
+    }
+
+
+
+    public boolean isReady() {
+        return isReady;
+    }
+
+
+
+    public void setReady(boolean isReady) {
+        this.isReady = isReady;
+    }
+
+    
+}
---
+++ src/FCGIRecord.java
@@ -0,0 +1,68 @@
+/*
+ * typedef struct {
+             unsigned char version;
+             unsigned char type;
+             unsigned char requestIdB1;
+             unsigned char requestIdB0;
+             unsigned char contentLengthB1;
+             unsigned char contentLengthB0;
+             unsigned char paddingLength;
+             unsigned char reserved;
+             unsigned char contentData[contentLength];
+             unsigned char paddingData[paddingLength];
+         } FCGI_Record;
+ */
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class FCGIRecord {
+    private int version;
+    private int type;
+    private int requestId;
+    private int contentLength;
+    private int paddingLength;
+    private ByteBuffer contentData;
+
+    public FCGIRecord(int version, int type, int requestId, int contentLength, int paddingLength, ByteBuffer contentData) {
+        this.version = version;
+        this.type = type;
+        this.requestId = requestId;
+        this.contentLength = contentLength;
+        this.paddingLength = paddingLength;
+        this.contentData = contentData;
+    }
+
+    @Override
+    public String toString() {
+        return "FCGIRecord [version=" + version + ", type=" + type + ", requestId=" + requestId + ", contentLength="
+                + contentLength + ", paddingLength=" + paddingLength + ", contentData=" + contentData.capacity()
+                + "]";
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public int getPaddingLength() {
+        return paddingLength;
+    }
+
+    public ByteBuffer getContentData() {
+        return contentData;
+    }
+
+    
+}
